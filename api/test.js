53c9f60fe08554b60af471eb6a52668ffe4d8684
James Fenn <18jafenn90@gmail.com>
2018-10-02 21:44:00 -0400

    formatted entire test response stuff

--- api/test.js
+++ api/test.js
@@ -1,3 +1,3 @@
 const _path = require("path");
 const _api = require(_path.resolve("./index.js"));
-console.log(_api.parseFile("index.js"));
+console.log(JSON.stringify(_api.parseFile("index.js"), null, 2));
